8844d4d6974bb7f2d953b4179d71ee240e33d6ae
tusharjoshi948@gmail.com <tusharjoshi948@gmail.com>
2021-07-28 14:04:44 +0530

    --- Auto-Git Commit ---

--- lambda.py
+++ lambda.py
@@ -5,7 +5,7 @@ print('Loading function')
 def lambda_handler(event, context):
     #1. Parse out query string params
     transactionId = event['queryStringParameters']['transactionId']
-    transactionId = event['queryStringParameters']['transactionId']
+    transactionType = event['queryStringParameters']['transactionId']
     transactionId = event['queryStringParameters']['transactionId']
 
 
